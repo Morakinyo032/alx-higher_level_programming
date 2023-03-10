45eea396a5b425f35fb0adaef57edc3712c9c01c
Morakinyo032 <ibraheemabdulwakeel55@gmail.com>
2023-03-10 09:27:45 +0100

    Fixing script

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
-def main():
-    pass
-
-
-import sys, calculator_1 as cal
-args = sys.argv
-size = len(args)
-if size != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
-a = int(args[1])
-b = int(args[3])
-c = args[2]
-flag = 0
-if c == '+':
-    print("{:d} + {:d} = {:d}".format(a, b, cal.add(a, b)))
-elif c == '-':
-    print("{:d} - {:d} = {:d}".format(a, b, cal.sub(a, b)))
-elif c == '*':
-    print("{:d} * {:d} = {:d}".format(a, b, cal.mul(a, b)))
-elif c == '/':
-    print("{:d} / {:d} = {:d}".format(a, b, cal.div(a, b)))
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
-    flag = 1
-if __name__ == " __main__":
-    main()
-if flag == 0:
-    sys.exit(0)
-else:
-    sys.exit(1)
+if __name__ == "__main__":
+    from calculator_1 import add, mul, div, sub
+    from sys import exit, argv
+    length = len(argv)
+    flag = 0;
+    if length != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        flag = 1
+    else:
+        a = int(argv[1])
+        b = argv[2]
+        c = int(argv[3])
+        op = add
+        operators = "+-/*"
+        if b == '*':
+            op = mul
+        elif b == '-':
+            op = sub
+        elif b == '/':
+            op = div
+        elif b == '+':
+            op = add
+        if b not in operators:
+            print("Unknown operator. Available operators: +, -, * and /")
+            flag = 1;
+        else:
+            print("{:d} {} {:d} = {:d}".format(a, b, c, op(a, c)))
+    if flag == 0:
+        exit(0)
+    else:
+        exit(1)
---
+++ 0x02-python-import_modules/cal_test.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+s = __import__('100-my_calculator')
